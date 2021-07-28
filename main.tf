9517b1a39db4a76fd59f23a5f1ebc89baeeb5dd7
fabioalexandrezup <fabio.alexandre@zup.com.br>
2021-07-28 12:34:29 -0300

    fix terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+
 resource "aws_s3_bucket" "squad_platform_tmp"{
     bucket  =   "orange-datalake-squad-platform"
-    acl     =   "private"
+    acl     =   "public-read"
     tags = {
         Name            =   "squad-platform"
         Environment     =   "Dev"
