026e0a660d0d881a668348d441f01c9a18c56e9a
Fabio Cordeiro <fcordeiro@wiser>
2021-07-28 00:19:25 -0300

    first test

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "squad_platform_tmp"{
+    bucket  =   "orange-datalake-squad-platform"
+    acl     =   "private"
+    tags = {
+        Name            =   "squad-platform"
+        Environment     =   "Dev"
+    }
+}
