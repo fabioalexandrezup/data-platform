a597c8a1918dc0ec0591dcc544b5726c0134474c
fabioalexandrezup <fabio.alexandre@zup.com.br>
2021-07-29 21:30:44 -0300

    second try

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
+
 provider "aws" {
-  version = "~> 2.0"
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "squad_platform_tmp"{
-    bucket  =   "orange-datalake-squad-platform"
-    acl     =   "public-read"
-    tags = {
-        Name            =   "squad-platform"
-        Environment     =   "Dev"
-    }
+resource "aws_s3_bucket" "squad_platform_tmp" {
+  bucket = "orange-datalake-squad-platform"
+  acl    = "public-read-write"
+  tags = {
+    Name        = "squad-platform"
+    Environment = "Dev"
+  }
 }
