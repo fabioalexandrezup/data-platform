9e992fb2caddcfecd82775c7674d65f16399329e
Fabio Cordeiro <fcordeiro@wiser>
2021-08-10 20:09:21 -0300

    testing github credentials

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAZFDKSPCMI2BH3GWT"
-  secret_key = "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"
 }
 
 resource "aws_s3_bucket" "squad_platform_tmp" {
